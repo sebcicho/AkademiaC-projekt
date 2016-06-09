5b3660ad88bc2f156fa9e96761e6fd07a4d21cc1
sebcicho <sebastian.cichocki1g@gmail.com>
2016-06-09 18:24:44 +0200

    main window cleanup and definition added

--- Tank/Tank/MainWindow.xaml.cs
+++ Tank/Tank/MainWindow.xaml.cs
@@ -25,19 +25,28 @@ namespace Tank
 
         PlayerTank playerTank = new PlayerTank(false) { XPosition = 270, XShootPosition = 270, YShootPosition =60, ShootVisible=0};
         Params gameParams = new Params() { level = 1, points = 0, health=100, reload=0};
+
         private int tankSpeed = 10;
         public int ticPrescaler =0;
-        public int ticInterval = 2;
+        public int ticMultiplier = 2;
 
+        public static int ticPrescalerMax = 1;
+        static public int barsLength = 150; //health and reloda bar length in pixels
+        static public int cannonOffset = 28; //offset of cannon position on canvas in pixels 
+        static public int playerShotRange = 500; 
+        static public int playerTankEndPosition = 60; 
+        static public int ammoVelocity = 10; //added with every tick to y shoot position
 
         public MainWindow()
         {
             InitializeComponent();
+
             PlayerTankObject.DataContext = playerTank;
             Level.DataContext = gameParams;
             Points.DataContext = gameParams;
             ReloadRectangle.DataContext = gameParams;
             HealthRectangle.DataContext = gameParams;
+
             PlayerAmmo.DataContext = playerTank;
             var timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(10);
@@ -50,39 +59,37 @@ namespace Tank
         {
 
             ticPrescaler++;
-            if (ticPrescaler>1)
+            if (ticPrescaler> ticPrescalerMax)
             {
                 ticPrescaler = 0;
                 
-                if (gameParams.reload <150)
+                if (gameParams.reload < barsLength)
                 {
-                    if (gameParams.reload <= 150- ticInterval)
-                        gameParams.reload+= ticInterval;
+                    if (gameParams.reload <= barsLength - ticMultiplier)
+                        gameParams.reload+= ticMultiplier;
                     else
-                        gameParams.reload = 150;
+                        gameParams.reload = barsLength;
                 }
-
-                
-
             }
+
             if (playerTank.shootFired == true)
             {
 
-                if (playerTank.YShootPosition == 60)
+                if (playerTank.YShootPosition == playerTankEndPosition)
                 {
-                    playerTank.XShootPosition = playerTank.XPosition+28;
+                    playerTank.XShootPosition = playerTank.XPosition+cannonOffset;
                     playerTank.ShootVisible = 100;
                 }
-                playerTank.YShootPosition += 10;
-                if (playerTank.YShootPosition == 500)
+                playerTank.YShootPosition += ammoVelocity;
+                if (playerTank.YShootPosition == playerShotRange)
                 {
                     playerTank.shootFired = false;
                     playerTank.XShootPosition = 0;
-                    playerTank.YShootPosition = 60;
+                    playerTank.YShootPosition = playerTankEndPosition;
                     playerTank.ShootVisible = 0;
                 }
             }
-
+  
         }
 
         private void MainWindow_OnKeyDown(object sender, KeyboardEventArgs e)
@@ -95,17 +102,13 @@ namespace Tank
                 gameParams.level++;
             if (Keyboard.IsKeyDown(Key.Space))
             {
-                if (gameParams.reload >= 150 && playerTank.shootFired == false)
+                if (gameParams.reload >= barsLength && playerTank.shootFired == false)
                 {
                     gameParams.reload = 0;
                     playerTank.shootFired = true;
                 }
             }
-
-
         }
-
-
 
     }
 }
